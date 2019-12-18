5dea3586ed9737482ee5d1ba701259ba41557cf0
Critter4Dinner <woffordben1992@gmail.com>
2019-12-18 09:58:11 -0500

    Adding a conditional render that shows that the joke is loading if it is taking a moment

--- src/components/App.js
+++ src/components/App.js
@@ -6,34 +6,49 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 
 class App extends React.Component {
- 
   state = {
+    loading: false,
     setup: "Are you ready",
     punchline: "for some jokes?"
-  }
+  };
 
-  getJoke = async (e) => {
-    e.preventDefault()
-    const api_call = await fetch('https://official-joke-api.appspot.com/random_joke')
-    const joke = await api_call.json()
+  getJoke = async e => {
+    this.setState({ loading: true });
+    e.preventDefault();
+    const api_call = await fetch(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    const joke = await api_call.json();
     this.setState({
+      loading: false,
       setup: joke.setup,
       punchline: joke.punchline
-    })
-  }
- 
-  render() {
+    });
+  };
 
-    return (
-      <div className="grid">
-        <CssBaseline />
-        <Setup setup={this.state.setup}/>
-        <Punchline punchline={this.state.punchline}/>
-        <div className="rowMaker">
-          <NewJokeButton getJoke={this.getJoke}/>
+  render() {
+    if (this.state.loading) {
+      return (
+        <div className="grid">
+          <CssBaseline />
+          <p>Loading..</p>
+          <div className="rowMaker">
+            <NewJokeButton getJoke={this.getJoke} />
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="grid">
+          <CssBaseline />
+          <Setup setup={this.state.setup} />
+          <Punchline punchline={this.state.punchline} />
+          <div className="rowMaker">
+            <NewJokeButton getJoke={this.getJoke} />
+          </div>
+        </div>
+      );
+    }
   }
 }
 
